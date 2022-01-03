958df3944bc869a00a230fccc83ca548ebd7233f
Aniruddha Inge <aniruddha.inge.2019@gmail.com>
2022-01-03 22:56:11 +0530

    Time: 2 ms (22.52%), Space: 38.1 MB (12.69%) - LeetHub

--- 263-ugly-number/263-ugly-number.java
+++ 263-ugly-number/263-ugly-number.java
@@ -1,9 +1,16 @@
 class Solution {
     public boolean isUgly(int num) {
-        for (int i=2; i<6 && num>0; i++){
-            while (num % i == 0)
-                num /= i;
+        if (num <= 0) {return false;}
+        if (num == 1) {return true;}
+        if (num % 2 == 0) {
+            return isUgly(num/2);
         }
-        return num == 1;
+        if (num % 3 == 0) {
+            return isUgly(num/3);
+        }
+        if (num % 5 == 0) {
+            return isUgly(num/5);
+        }
+        return false;
     }
 }
