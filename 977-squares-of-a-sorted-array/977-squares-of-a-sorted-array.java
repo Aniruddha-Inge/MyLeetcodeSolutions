25493d84ebc45e233bbf4c2db48230bd6961ee90
Aniruddha Inge <aniruddha.inge.2019@gmail.com>
2022-02-03 18:13:48 +0530

    Time: 2 ms (74.49%), Space: 56.3 MB (5.05%) - LeetHub

--- 977-squares-of-a-sorted-array/977-squares-of-a-sorted-array.java
+++ 977-squares-of-a-sorted-array/977-squares-of-a-sorted-array.java
@@ -1,9 +1,24 @@
 class Solution {
     public int[] sortedSquares(int[] nums) {
-        for(int i=0; i<nums.length; i++){
-            nums[i] = nums[i]*nums[i];
+        // initialize a new output array with all the 0s and traverse from the right. 
+        //Use 2 pointers, 1 at the start and one at the end of the nums array
+        //Compare the absolute value of the elements of the nums array
+        //whichever is greater add its squared value at the end of the output array and move the pointers accordingly
+        
+        int[] ans = new int[nums.length];
+        int start = 0;
+        int end = nums.length-1;
+        
+        for(int i=nums.length-1; i>=0; i--){
+            if(Math.abs(nums[start]) > Math.abs(nums[end])){
+                ans[i] = nums[start] * nums[start];
+                start++;
+            }
+            else{
+               ans[i] = nums[end] * nums[end];
+                end--;
+            }
         }
-        Arrays.sort(nums);
-        return nums;
+        return ans;
     }
 }
