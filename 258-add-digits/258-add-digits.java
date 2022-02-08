b81f55962d0d332c2f673655e323b19203bc7f2a
Aniruddha Inge <aniruddha.inge.2019@gmail.com>
2022-02-08 14:26:50 +0530

    Time: 2 ms (59.15%), Space: 41.1 MB (13.44%) - LeetHub

--- 258-add-digits/258-add-digits.java
+++ 258-add-digits/258-add-digits.java
@@ -1,16 +1,7 @@
 class Solution {
     public int addDigits(int num) {
-    int result = num;
-    
-    while (result > 9) {
-        result = 0;
-        while (num > 0) {
-            result += num % 10;
-            num /= 10;
-        }
-        num = result;
+        if(num == 0) return 0;
+        else if(num % 9 == 0) return 9;
+        else return num % 9;
     }
-    
-    return result;
-}
 }
