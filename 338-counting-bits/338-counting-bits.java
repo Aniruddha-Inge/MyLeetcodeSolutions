4063155a1e981eb7a87281ce5d0fcf57143a3cbe
Aniruddha Inge <aniruddha.inge.2019@gmail.com>
2022-03-01 09:10:43 +0530

    Time: 23 ms (6.93%), Space: 48.4 MB (19.74%) - LeetHub

---
+++ 338-counting-bits/338-counting-bits.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int[] countBits(int n) {
+        int[] ans = new int[n+1];
+        for(int i=0; i<n+1; i++){
+            ans[i] = solve(i);
+        }
+        return ans;
+    }
+    
+    public int solve(int n){
+        if(n == 0) return 0;
+        if(n == 1) return 1;
+        
+        if(n%2 == 0) return solve(n/2);
+        else return 1 + solve(n/2);
+    }
+}
