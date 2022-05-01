4f37597c4899e53f8e09cfb1f368c69fbda5e2c4
Aniruddha Inge <aniruddha.inge.2019@gmail.com>
2022-05-01 15:08:51 +0530

    Time: 1 ms (64.33%), Space: 42.2 MB (42.07%) - LeetHub

--- 709-to-lower-case/709-to-lower-case.java
+++ 709-to-lower-case/709-to-lower-case.java
@@ -1,5 +1,15 @@
 class Solution {
     public String toLowerCase(String s) {
-        return s.toLowerCase();
+        StringBuilder sb = new StringBuilder(s);
+        
+        for(int i=0; i<sb.length(); i++){
+            char ch = sb.charAt(i);
+            
+            if(ch >= 'A' && ch <='Z'){
+                char toLower = (char)('a' + ch - 'A');
+                sb.setCharAt(i, toLower);
+            }
+        }
+        return sb.toString();
     }
 }
