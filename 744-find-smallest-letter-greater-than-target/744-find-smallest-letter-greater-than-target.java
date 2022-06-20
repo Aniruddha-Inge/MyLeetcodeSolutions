a9bca371cb357dd1581677213016c7243c189c32
Aniruddha Inge <aniruddha.inge.2019@gmail.com>
2022-06-20 18:36:44 +0530

    Time: 0 ms (100.00%), Space: 42.6 MB (89.20%) - LeetHub

--- 744-find-smallest-letter-greater-than-target/744-find-smallest-letter-greater-than-target.java
+++ 744-find-smallest-letter-greater-than-target/744-find-smallest-letter-greater-than-target.java
@@ -5,11 +5,12 @@ class Solution {
         
         while(start <= end){
             int mid = start + (end - start)/2;
+            
             if(target < letters[mid]){
-                end = mid - 1;
+                end = mid-1;
             }
-            else {
-                start = mid + 1;
+            else{
+                start = mid+1;
             }
         }
         return letters[start % letters.length];
