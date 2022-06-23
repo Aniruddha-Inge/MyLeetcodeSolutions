3406f293c0dc5a5f6669f9f9e0e6ef0de3e0d9f4
Aniruddha Inge <aniruddha.inge.2019@gmail.com>
2022-06-23 14:50:12 +0530

    Time: 8 ms (54.53%), Space: 75.1 MB (74.44%) - LeetHub

--- 287-find-the-duplicate-number/287-find-the-duplicate-number.java
+++ 287-find-the-duplicate-number/287-find-the-duplicate-number.java
@@ -1,17 +1,26 @@
 class Solution {
     public int findDuplicate(int[] nums) {
-        int slow = 0;
-        int fast = 0;
+        // we can solve this question with 2 methods
+        //1st one is cyclic sort since given integer range in 1 to n
+        //2nd method is slow fast pointers (same as LinkedList)
         
-        do{
-            slow = nums[slow];
-            fast = nums[nums[fast]];
-        } while(slow != fast);
-            slow = 0;
-            while(slow != fast){
-                slow = nums[slow];
-                fast = nums[fast];
-            }
-        return slow;
+        int i=0;
+        while(i<nums.length){
+            
+            if(nums[i] != i+1){
+                int correct = nums[i] - 1;
+                if(nums[i] != nums[correct]){
+                    swap(nums, i, correct);
+                }
+                else return nums[i];
+            } else i++;
+            
+        }
+        return -1;
+    }
+    void swap(int[] arr, int a, int b){
+        int temp = arr[a];
+        arr[a] = arr[b];
+        arr[b] = temp;
     }
 }
