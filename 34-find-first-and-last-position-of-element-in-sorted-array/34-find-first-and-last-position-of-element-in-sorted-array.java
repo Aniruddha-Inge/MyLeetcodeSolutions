2fea4aae51a7f2aa4a8301cd1cab506e23cec0e2
Aniruddha Inge <aniruddha.inge.2019@gmail.com>
2022-08-06 15:58:19 +0530

    Time: 1 ms (40.30%), Space: 47.1 MB (68.03%) - LeetHub

--- 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.java
+++ 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,49 +1,45 @@
 class Solution {
     public int[] searchRange(int[] nums, int target) {
-        int first = firstElement(nums,target);
-        int last = lastElement(nums,target);
+        int first = firstIndex(nums, target);
+        int last = lastIndex(nums, target);
         
-        return new int[]{first,last};
-    
+        return new int[]{first, last};
     }
-    static int firstElement(int[] arr,int target){
-            int start = 0;
-            int end = arr.length-1;
-            int res = -1;
-            while(start<=end){
-                int mid = start + (end-start)/2;
-                if(target == arr[mid]){
-                    res = mid;
-                    end = mid-1;
-                }
-                else if(target<arr[mid]){
-                    end = mid-1;
-                }
-                else{
-                    start = mid+1;
-                }
+    public int firstIndex(int[] nums, int target){
+        int start = 0;
+        int end = nums.length-1;
+        int res = -1;
+        
+        while(start <= end){
+            int mid = start + (end - start)/2;
+            if(target == nums[mid]){
+                res = mid;
+                end = mid-1;
             }
-            return res;
-        }
-    // return new int[]{-1,-1};
-        static int lastElement(int[] arr,int target){
-            int start = 0;
-            int end = arr.length-1;
-            int res = -1;
-            while(start<=end){
-                int mid = start + (end-start)/2;
-                if(target == arr[mid]){
-                    res = mid;
-                    start = mid+1;
-                }
-                else if(target<arr[mid]){
-                    end = mid-1;
-                }
-                else{
-                    start = mid+1;
-                }
+            else if(target < nums[mid]){
+                end = mid-1;
             }
-            return res;
+            else start = mid+1;
         }
-    // return new int[]{-1,-1};
+        return res;
+    }
+    
+    public int lastIndex(int[] nums, int target){
+        int start = 0;
+        int end = nums.length-1;
+        int res = -1;
+        
+        while(start <= end){
+            int mid = start + (end - start)/2;
+            if(target == nums[mid]){
+                res = mid;
+                start = mid+1;
+            }
+            else if(target < nums[mid]){
+                end = mid-1;
+            }
+            else start = mid+1;
+        }
+        return res;
+    }
 }
