58ddea0bcd326ee0bfdd9844b07e40b42321e8a3
Aniruddha Inge <aniruddha.inge.2019@gmail.com>
2022-08-09 09:01:02 +0530

    Time: 159 ms (18.20%), Space: 74.4 MB (5.04%) - LeetHub

--- 633-sum-of-square-numbers/633-sum-of-square-numbers.java
+++ 633-sum-of-square-numbers/633-sum-of-square-numbers.java
@@ -1,20 +1,9 @@
 class Solution {
     public boolean judgeSquareSum(int c) {
-        long start = 0;
-        long end = (long)(Math.sqrt(c));
-        
-        while(start <= end){
-            long mid = start + (end - start)/2;
-            
-            long ans = start*start + end*end;
-            
-            if(ans == c) return true;
-            else if(ans > c){
-                end--;
-            }
-            else{
-                start++;
-            }
+        HashSet<Integer> set = new HashSet<>();
+        for(int i=0; i<= Math.sqrt(c); i++){
+            set.add(i*i);
+            if(set.contains(c-i*i)) return true;
         }
         return false;
     }
